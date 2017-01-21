465e03ff2bf5f8b93487e3de587ec3ab03c2e3a8
Carlo <carloalbino@gmail.com>
2017-01-21 13:47:37 -0500

    WordZone mechanic complete

--- TyperWalker/Assets/Scripts/LetterParticle.cs
+++ TyperWalker/Assets/Scripts/LetterParticle.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class LetterParticle : MonoBehaviour {
 
     public float m_speed = 1.0f;
-    public Text m_letter = null;
+    public Text m_letterText = null;
     private Vector2 m_randomDir = Vector2.zero;
     private Rigidbody2D m_rb = null;
+    private bool m_destroyAfterTime = true;
+    [SerializeField]
+    private float m_destructionTime = 1.2f;
+    private string m_letter = "";
     
     void Start()
     {
@@ -16,7 +20,7 @@ public class LetterParticle : MonoBehaviour {
         m_randomDir.x = Random.Range(-0.8f, 0.8f);
         m_randomDir.y = Random.Range(-0.8f, 0.8f);
         m_rb.AddForce(Vector2.up + m_randomDir * m_speed, ForceMode2D.Impulse);
-        Destroy(this.gameObject, 1.2f);
+        StartCoroutine(DestroyAfterTime());
     }
 
     void Update()
@@ -24,8 +28,47 @@ public class LetterParticle : MonoBehaviour {
         //transform.Translate((Vector2.up + m_randomDir) * m_speed * Time.deltaTime);
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        WordZone wz = other.GetComponent<WordZone>();
+        if(wz != null)
+        {
+            wz.CheckForWord(this);
+        }
+    }
+
     public void SetLetter(string letter)
     {
-        m_letter.text = letter;
+        m_letterText.text = letter;
+        m_letter = letter;
+    }
+
+    public void StopDestruction()
+    {
+        m_destroyAfterTime = false;
+    }
+
+    public string GetLetter()
+    {
+        return m_letter;
+    }
+
+    public void SetLayer(LayerMask layer)
+    {
+        this.gameObject.layer = layer;
+    }
+
+    public void SetColour(Color c)
+    {
+        m_letterText.color = c;
+    }
+
+    private IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(m_destructionTime);
+        if (m_destroyAfterTime)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ TyperWalker/Assets/Scripts/WordZone.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordZone : MonoBehaviour {
+
+    [SerializeField]
+    private string m_word = "empty";
+    [SerializeField]
+    private Color m_wordColour = Color.white;
+    private List<LetterParticle> m_letters = new List<LetterParticle>();
+    private bool m_wordComplete = false;
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+        if (m_word.Length <= 0)
+        {
+            foreach(LetterParticle l in m_letters)
+            {
+                l.SetColour(m_wordColour);
+            }
+        }
+	}
+
+    public void CheckForWord(LetterParticle l)
+    {
+        if (l != null)
+        {
+            for (int i = 0; i < m_word.Length; i++)
+            {
+                if (m_word.ToLower()[i] == l.GetLetter()[0])
+                {
+                    l.StopDestruction();
+                    l.SetLayer(10);
+                    m_letters.Add(l);
+                    string tempWord = "";
+                    foreach(char c in m_word.ToLower())
+                    {
+                        if(c != l.GetLetter()[0])
+                        {
+                            tempWord += c;
+                        }
+                    }
+                    m_word = tempWord;
+                    break;
+                }
+            }
+        }
+    }
+}
